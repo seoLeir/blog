893ddaf7b22ae52c8312cbc68cb554eac8084eb6
Miralim Mirzayev <mirzayevmiralim28@gmail.com>
2023-09-14 17:02:20 +0500

    Init commit

---
+++ src/test/java/io/seoLeir/socialmedia/SocialMediaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.seoLeir.socialmedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
