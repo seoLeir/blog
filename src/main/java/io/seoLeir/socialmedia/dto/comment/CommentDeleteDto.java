9c27798cfdbcb15134b6d437103fb2e6b2f8ec1c
Mirolim Mirzayev <mirzayevmiralim28@gmail.com>
2023-11-11 18:12:12 +0500

    Introduce publication comment validation to ensure data integrity and enhance user experience

--- src/main/java/io/seoLeir/socialmedia/dto/comment/CommentDeleteDto.java
+++ src/main/java/io/seoLeir/socialmedia/dto/comment/CommentDeleteDto.java
@@ -2,5 +2,6 @@ package io.seoLeir.socialmedia.dto.comment;
 
 import java.util.UUID;
 
-public record CommentDeleteDto(UUID commentUuid) {
+public record CommentDeleteDto(
+        @org.hibernate.validator.constraints.UUID UUID commentUuid) {
 }
---
+++ src/main/java/io/seoLeir/socialmedia/validation/comment/CommentText.java
@@ -0,0 +1,24 @@
+package io.seoLeir.socialmedia.validation.comment;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.FIELD, ElementType.PARAMETER, ElementType.RECORD_COMPONENT})
+@Constraint(validatedBy = { })
+@Size(min = 1, max = 256, message = "Comment text cannot exceed 256 characters")
+@NotEmpty(message = "Comment text should not be blank")
+public @interface CommentText {
+    String message() default "Invalid comment text";
+
+    Class<?>[] groups() default { };
+
+    Class<? extends Payload>[] payload() default { };
+}
